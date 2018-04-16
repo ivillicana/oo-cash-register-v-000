1c43a5c0fae4804071232ba830cdf335558a06b2
Isaac Villicana <villicana.i@gmail.com>
2018-04-16 23:26:10 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,9 @@ class CashRegister
   end
 
   def add_item(title, price, quantity = 1)
-    @items << title
+    quantity.times do
+      @items << title
+    end
     @total += price * quantity
   end
 
